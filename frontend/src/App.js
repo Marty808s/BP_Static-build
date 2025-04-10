a52261ca0999a01a2e4203b140b0667ae5b4f8c0
Marty808s <martybeats.cz@gmail.com>
2025-04-10 11:47:01 +0200

    tailwind ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+         <p className='text-black'>asdssasd</p>
+        <button className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+          onClick = {() => console.log("Tailwind je ready!!")}
         >
-          Learn React
-        </a>
+          Button
+      </button>
       </header>
     </div>
   );
