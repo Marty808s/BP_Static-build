894992c17d4631db3d5f3e552832a513bbc23cd7
Marty808s <martybeats.cz@gmail.com>
2025-04-30 10:59:24 +0200

    Create UserEntity.js
    TO:DO - podle poznámek dodělat entity pro uživatele - vstupy budou dicts

---
+++ frontend/src/components/Users/UserEntity.js
@@ -0,0 +1,17 @@
+// zde bude entity pro jakéhokoliv uživatele v systému, které budou mít společné vlastnosti
+// -> tlačítka a na ně akce
+// -> parametr co vykreslit
+// -> parametr na barvu pozadí (podle stavu entity)
+import Button from "@core/Button/Button";
+import Container from "@core/Container/Container";
+import Paragraph from "@core/Text/Paragraph";
+import Headings from "@core/Text/Headings";
+import { ContainerForEntity } from "@core/Container/ContainerForEntity";
+
+export default function UserEntity({entity, onClick, onView}) {
+
+    return (
+        <ContainerForEntity>
+        </ContainerForEntity>
+    )
+}
